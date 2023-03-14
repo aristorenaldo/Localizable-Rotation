b6d004e8f0719007191b12891db3654801e53b2e
aristo <aristo1renaldo@gmail.com>
2023-03-15 01:08:07 +0700

    fix models name

--- imagenet/train_moe.py
+++ imagenet/train_moe.py
@@ -24,7 +24,8 @@ import models
 model_names = sorted(name for name in models.__dict__
     if not name.startswith("__")
     and callable(models.__dict__[name])
-    and not 'Nomoe') 
+    and name != 'Nomoe') 
+
 backbone_names = [
     "resnet18",
     "resnet34",
